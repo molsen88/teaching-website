ec6338c15d97b854b464c2a9fc19b8eae91acaae
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-08-16 21:31:49 -0600

    carousel working

--- src/components/ClassCarousel.jsx
+++ src/components/ClassCarousel.jsx
@@ -14,14 +14,16 @@ function ClassCarousel() {
             <Col>
                 <Carousel >
                     <Carousel.Item>
-                        <Image src={require( '../images/Sun Valley Photo copy.jpeg' )} className='carousel-image' rounded />
-                        <Carousel.Caption>
-                            <h3 className="carousel-labels">First slide label</h3>
-                            <p className="carousel-description">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <Image src={require( '../images/Front Room Photo.jpg' )} className='carousel-image' rounded />
+                        < Carousel.Caption >
+                            <div className="first-slide-section">
+                                <h3 className="carousel-labels">First slide label</h3>
+                                <p className="carousel-description">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </div>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <Image src={require( '../images/Mohamed-Salah-wallpaper-HD.jpg' )} className='carousel-image' rounded />
+                        <Image src={require( '../images/Reading Corner.jpg' )} className='carousel-image' rounded />
 
                         <Carousel.Caption>
                             <h3>Second slide label</h3>
@@ -29,21 +31,21 @@ function ClassCarousel() {
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <Image src={require( '../images/Mohamed-Salah-wallpaper-HD.jpg' )} className='carousel-image' rounded />
+                        <Image src={require( '../images/Leadership Wall.jpg' )} className='carousel-image' rounded />
                         <Carousel.Caption>
                             <h3>Third slide label</h3>
                             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <Image src={require( '../images/Sun Valley Photo copy.jpeg' )} className='carousel-image' rounded />
+                        <Image src={require( '../images/Sunset Beach.jpg' )} className='carousel-image' rounded />
                         <Carousel.Caption>
                             <h3>First slide label</h3>
                             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <Image src={require( '../images/Sun Valley Photo copy.jpeg' )} className='carousel-image' rounded />
+                        <Image src={require( '../images/Mountains.jpg' )} className='carousel-image' rounded />
                         <Carousel.Caption>
                             <h3>First slide label</h3>
                             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
